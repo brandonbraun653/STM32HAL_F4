dd1a552b3d04a15a59605e5c52079254faf89474
Brandon Braun <brandonbraun653@gmail.com>
2019-04-30 21:17:09 -0500

    Updated some mocks. Added clangformat file.

--- mock/mock_stm32_hal_dma.cpp
+++ mock/mock_stm32_hal_dma.cpp
@@ -12,78 +12,95 @@
 
 #if defined( GMOCK_TEST )
 
-STM32_HAL_DMA_Mock *STM32_HAL_DMA_MockObj = nullptr;
+namespace STM32HAL_Mock
+{
+  using ::testing::_;
+  using ::testing::Return;
+
+  DMANiceMock_sPtr dmaMockObj = nullptr;
+
+  DMAMock::DMAMock()
+  {
+    ON_CALL( *this, HAL_DMA_Init( _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_DeInit( _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_Start( _, _, _, _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_Start_IT( _, _, _, _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_Abort( _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_Abort_IT( _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_PollForTransfer( _, _, _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_CleanCallbacks( _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_RegisterCallback( _, _, _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_UnRegisterCallback( _, _ ) ).WillByDefault( Return( HAL_OK ) );
+    ON_CALL( *this, HAL_DMA_GetState( _ ) ).WillByDefault( Return( HAL_DMA_STATE_READY ) );
+    ON_CALL( *this, HAL_DMA_GetError( _ ) ).WillByDefault( Return( 0u ) );
+  }
+}    // namespace STM32HAL_Mock
 
 HAL_StatusTypeDef HAL_DMA_Init( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_Init( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_Init( hdma );
 }
 
 HAL_StatusTypeDef HAL_DMA_DeInit( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_DeInit( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_DeInit( hdma );
 }
 
-HAL_StatusTypeDef HAL_DMA_Start( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress,
-                                                            uint32_t DataLength )
+HAL_StatusTypeDef HAL_DMA_Start( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_Start( hdma, SrcAddress, DstAddress, DataLength );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_Start( hdma, SrcAddress, DstAddress, DataLength );
 }
 
-HAL_StatusTypeDef HAL_DMA_Start_IT( DMA_HandleTypeDef *hdma, uint32_t SrcAddress,
-                                                               uint32_t DstAddress, uint32_t DataLength )
+HAL_StatusTypeDef HAL_DMA_Start_IT( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_Start_IT( hdma, SrcAddress, DstAddress, DataLength );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_Start_IT( hdma, SrcAddress, DstAddress, DataLength );
 }
 
 HAL_StatusTypeDef HAL_DMA_Abort( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_Abort( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_Abort( hdma );
 }
 
 HAL_StatusTypeDef HAL_DMA_Abort_IT( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_Abort_IT( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_Abort_IT( hdma );
 }
 
-HAL_StatusTypeDef HAL_DMA_PollForTransfer( DMA_HandleTypeDef *hdma,
-                                                                      HAL_DMA_LevelCompleteTypeDef CompleteLevel,
-                                                                      uint32_t Timeout )
+HAL_StatusTypeDef HAL_DMA_PollForTransfer( DMA_HandleTypeDef *hdma, HAL_DMA_LevelCompleteTypeDef CompleteLevel,
+                                           uint32_t Timeout )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_PollForTransfer( hdma, CompleteLevel, Timeout );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_PollForTransfer( hdma, CompleteLevel, Timeout );
 }
 
 void HAL_DMA_IRQHandler( DMA_HandleTypeDef *hdma )
 {
-  STM32_HAL_DMA_MockObj->HAL_DMA_IRQHandler( hdma );
+  STM32HAL_Mock::dmaMockObj->HAL_DMA_IRQHandler( hdma );
 }
 
 HAL_StatusTypeDef HAL_DMA_CleanCallbacks( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_CleanCallbacks( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_CleanCallbacks( hdma );
 }
 
-HAL_StatusTypeDef HAL_DMA_RegisterCallback( DMA_HandleTypeDef *hdma,
-                                                                       HAL_DMA_CallbackIDTypeDef CallbackID,
-                                                                       void ( *pCallback )( DMA_HandleTypeDef *_hdma ) )
+HAL_StatusTypeDef HAL_DMA_RegisterCallback( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID,
+                                            void ( *pCallback )( DMA_HandleTypeDef *_hdma ) )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_RegisterCallback( hdma, CallbackID, pCallback );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_RegisterCallback( hdma, CallbackID, pCallback );
 }
 
-HAL_StatusTypeDef HAL_DMA_UnRegisterCallback( DMA_HandleTypeDef *hdma,
-                                                                         HAL_DMA_CallbackIDTypeDef CallbackID )
+HAL_StatusTypeDef HAL_DMA_UnRegisterCallback( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_UnRegisterCallback( hdma, CallbackID );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_UnRegisterCallback( hdma, CallbackID );
 }
 
 HAL_DMA_StateTypeDef HAL_DMA_GetState( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_GetState( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_GetState( hdma );
 }
 
 uint32_t HAL_DMA_GetError( DMA_HandleTypeDef *hdma )
 {
-  return STM32_HAL_DMA_MockObj->HAL_DMA_GetError( hdma );
+  return STM32HAL_Mock::dmaMockObj->HAL_DMA_GetError( hdma );
 }
 
 #endif /* GMOCK_TEST */
--- mock/mock_stm32_hal_dma.hpp
+++ mock/mock_stm32_hal_dma.hpp
@@ -32,52 +32,63 @@ extern "C"
 }
 #endif
 
-class STM32_HAL_DMA_Interface
+namespace STM32HAL_Mock
 {
-public:
-  virtual ~STM32_HAL_DMA_Interface() = default;
+  class DMAInterface
+  {
+  public:
+    virtual ~DMAInterface() = default;
 
-  virtual HAL_StatusTypeDef HAL_DMA_Init( DMA_HandleTypeDef *hdma );
-  virtual HAL_StatusTypeDef HAL_DMA_DeInit( DMA_HandleTypeDef *hdma );
-  virtual HAL_StatusTypeDef HAL_DMA_Start( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength );
-  virtual HAL_StatusTypeDef HAL_DMA_Start_IT( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength );
-  virtual HAL_StatusTypeDef HAL_DMA_Abort( DMA_HandleTypeDef *hdma );
-  virtual HAL_StatusTypeDef HAL_DMA_Abort_IT( DMA_HandleTypeDef *hdma );
-  virtual HAL_StatusTypeDef HAL_DMA_PollForTransfer( DMA_HandleTypeDef *hdma, HAL_DMA_LevelCompleteTypeDef CompleteLevel, uint32_t Timeout );
-  virtual void HAL_DMA_IRQHandler( DMA_HandleTypeDef *hdma );
-  virtual HAL_StatusTypeDef HAL_DMA_CleanCallbacks( DMA_HandleTypeDef *hdma );
-  virtual HAL_StatusTypeDef HAL_DMA_RegisterCallback( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID, void ( *pCallback )( DMA_HandleTypeDef *_hdma ) );
-  virtual HAL_StatusTypeDef HAL_DMA_UnRegisterCallback( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID );
-  virtual HAL_DMA_StateTypeDef HAL_DMA_GetState( DMA_HandleTypeDef *hdma );
-  virtual uint32_t HAL_DMA_GetError( DMA_HandleTypeDef *hdma );
-};
+    virtual HAL_StatusTypeDef HAL_DMA_Init( DMA_HandleTypeDef *hdma ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_DeInit( DMA_HandleTypeDef *hdma ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_Start( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress,
+                                             uint32_t DataLength ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_Start_IT( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress,
+                                                uint32_t DataLength ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_Abort( DMA_HandleTypeDef *hdma ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_Abort_IT( DMA_HandleTypeDef *hdma ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_PollForTransfer( DMA_HandleTypeDef *hdma, HAL_DMA_LevelCompleteTypeDef CompleteLevel,
+                                                       uint32_t Timeout ) = 0;
+    virtual void HAL_DMA_IRQHandler( DMA_HandleTypeDef *hdma ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_CleanCallbacks( DMA_HandleTypeDef *hdma ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_RegisterCallback( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID,
+                                                        void ( *pCallback )( DMA_HandleTypeDef *_hdma ) ) = 0;
+    virtual HAL_StatusTypeDef HAL_DMA_UnRegisterCallback( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID ) = 0;
+    virtual HAL_DMA_StateTypeDef HAL_DMA_GetState( DMA_HandleTypeDef *hdma ) = 0;
+    virtual uint32_t HAL_DMA_GetError( DMA_HandleTypeDef *hdma ) = 0;
+  };
 
-class STM32_HAL_DMA_Mock : public STM32_HAL_DMA_Interface
-{
-public:
-  virtual ~STM32_HAL_DMA_Mock() = default;
+  class DMAMock : public DMAInterface
+  {
+  public:
+    DMAMock();
+    virtual ~DMAMock() = default;
 
-  MOCK_METHOD1( HAL_DMA_Init, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD1( HAL_DMA_DeInit, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD4( HAL_DMA_Start,
-                HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength ) );
-  MOCK_METHOD4( HAL_DMA_Start_IT,
-                HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength ) );
-  MOCK_METHOD1( HAL_DMA_Abort, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD1( HAL_DMA_Abort_IT, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD3( HAL_DMA_PollForTransfer,
-                HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, HAL_DMA_LevelCompleteTypeDef CompleteLevel, uint32_t Timeout ) );
-  MOCK_METHOD1( HAL_DMA_IRQHandler, void( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD1( HAL_DMA_CleanCallbacks, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD3( HAL_DMA_RegisterCallback, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID,
-                                                             void ( *pCallback )( DMA_HandleTypeDef *_hdma ) ) );
-  MOCK_METHOD2( HAL_DMA_UnRegisterCallback,
-                HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID ) );
-  MOCK_METHOD1( HAL_DMA_GetState, HAL_DMA_StateTypeDef( DMA_HandleTypeDef *hdma ) );
-  MOCK_METHOD1( HAL_DMA_GetError, uint32_t( DMA_HandleTypeDef *hdma ) );
-};
+    MOCK_METHOD1( HAL_DMA_Init, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD1( HAL_DMA_DeInit, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD4( HAL_DMA_Start,
+                  HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength ) );
+    MOCK_METHOD4( HAL_DMA_Start_IT,
+                  HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, uint32_t SrcAddress, uint32_t DstAddress, uint32_t DataLength ) );
+    MOCK_METHOD1( HAL_DMA_Abort, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD1( HAL_DMA_Abort_IT, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD3( HAL_DMA_PollForTransfer,
+                  HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, HAL_DMA_LevelCompleteTypeDef CompleteLevel, uint32_t Timeout ) );
+    MOCK_METHOD1( HAL_DMA_IRQHandler, void( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD1( HAL_DMA_CleanCallbacks, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD3( HAL_DMA_RegisterCallback, HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID,
+                                                               void ( *pCallback )( DMA_HandleTypeDef *_hdma ) ) );
+    MOCK_METHOD2( HAL_DMA_UnRegisterCallback,
+                  HAL_StatusTypeDef( DMA_HandleTypeDef *hdma, HAL_DMA_CallbackIDTypeDef CallbackID ) );
+    MOCK_METHOD1( HAL_DMA_GetState, HAL_DMA_StateTypeDef( DMA_HandleTypeDef *hdma ) );
+    MOCK_METHOD1( HAL_DMA_GetError, uint32_t( DMA_HandleTypeDef *hdma ) );
+  };
 
-extern STM32_HAL_DMA_Mock *STM32_HAL_DMA_MockObj;
+  using DMANiceMock      = ::testing::NiceMock<DMAMock>;
+  using DMANiceMock_sPtr = std::shared_ptr<DMANiceMock>;
+
+  extern DMANiceMock_sPtr dmaMockObj;
+}    // namespace STM32HAL_Mock
 
 #endif /* GMOCK_TEST */
 
